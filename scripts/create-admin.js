b80bc338052fc3bceadf947740ad85ed448d0b6c
PassiveCoderOfficial <passivecoder.com@gmail.com>
2026-01-21 17:49:43 +0600

    Public Home Page Pricing and Features added

--- scripts/create-admin.js
+++ scripts/create-admin.js
@@ -4,12 +4,12 @@ const bcrypt = require("bcryptjs");
 
 async function main() {
   const prisma = new PrismaClient();
-  const password = "Itsn0t@p@ssw0rd"; // change this
+  const password = "P@ssiveC0der"; // change this
   const hashed = await bcrypt.hash(password, 10);
 
   const user = await prisma.user.create({
     data: {
-      email: "expertnearme@gmail.com",
+      email: "walibdpro@gmail.com",
       password: hashed,
       role: "ADMIN",
       verified: true,
